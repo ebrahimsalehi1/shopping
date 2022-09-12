c959e4ff0e29d2fe10cdeb42dfe89de792885a84
Ebrahim <eeeee.bbbbb.iiiii@gmail.com>
2022-09-12 18:25:55 +0200

    Ebrahim second commit

--- data_provider.py
+++ data_provider.py
@@ -14,5 +14,5 @@ order3 = Order(date=func.date('2022-09-15'), customer_id=2)
 db.session.add_all([customer1, customer2, customer3])
 db.session.add_all([order1, order2, order3])
 
-
+# Commit must be done
 db.session.commit()
